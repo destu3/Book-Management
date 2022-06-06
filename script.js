262580639d0ceb1c3cb291e39bb476e5f5e58152
destu3 <obose03@gmail.com>
2022-06-06 10:01:24 +0100

    added read status toggle

--- script.js
+++ script.js
@@ -6,31 +6,31 @@ function Book(title, author, pages, read){
     this.read = read
 }
 
-Book.prototype.printDetails = function(){
-    let readStatus = ""
-        if (this.read == false){
-            readStatus = "has not been read yet"
-        }else{
-            readStatus = "has been read"
-        }
-        return (`${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`)
+Book.prototype.toggleReadStatus = function(){
+    if (this.read == "Completed"){
+        this.read = "Not Completed"
+    }
+    else if(this.read == "Not Completed"){
+        this.read = "Completed"
+    }
 }
 
-// book input selectors
+// constants
 const bookName = document.getElementById("name")
 const bookAuthor = document.getElementById("author")
 const numberOfPages = document.getElementById("pages")
-const bookCompleted = document.getElementById("read")
+const bookCompleted = document.getElementById("completed")
 const errorMessage = document.querySelector(".error")
 const addBookBtn = document.getElementById("addBtn")
 const bookCard = document.querySelector(".book-card")
+const bookGrid = document.querySelector(".book-view") 
 
 // library array 
 // dummy content
-let book1 = new Book("Nightwork", "Nora Roberts", "798", true)
-let book2 = new Book("The Summer Place", "Jennifer Weiner", "563", false)
-let book3 = new Book("22 Seconds", "James Patterson", "638", true)
-let book4 = new Book("Dream Town", "David Baldacci", "975", false)
+let book1 = new Book("Nightwork", "Nora Roberts", "798", "Not Completed")
+let book2 = new Book("The Summer Place", "Jennifer Weiner", "563", "Completed")
+let book3 = new Book("22 Seconds", "James Patterson", "638", "Not Completed")
+let book4 = new Book("Dream Town", "David Baldacci", "975", "Completed")
 
 let theLibrary = [
     book1,
@@ -42,46 +42,19 @@ let theLibrary = [
 // adding books functionality
 addBookBtn.addEventListener("click", addBooksToLibrary)
 
-function addBooksToLibrary(){
-    if(bookName.value.length == 0 || bookAuthor.value.length == 0 || numberOfPages.value.length == 0 || bookCompleted.value.length == 0){
-        errorMessage.textContent = "Fill in all fields" 
-        errorMessage.classList.remove("positive-e")
-    }
-    else{
-        errorMessage.textContent = "Book successfully added" 
-        errorMessage.classList.add("positive-e")
-        let book = new Book(bookName.value, bookAuthor.value, Number(numberOfPages.value), bookCompleted.value)
-        theLibrary.push(book)
-        createBook(book)
-    }
-}
 
-const bookGrid = document.querySelector(".book-view") 
-
-
+// functions
 function populateBookGrid(){
         theLibrary.forEach((book) => {
         createBook(book)
     })
 }
 
-window.addEventListener("DOMContentLoaded", populateBookGrid())
-
-// delete book from library
-
 function deleteBook(index){
     theLibrary.splice(index,1)
     refresh()
     populateBookGrid()
 }
-
-let deleteBtn = document.querySelectorAll(".delete-btn")
-
-// deleteBtn.forEach(btn => {
-//     btn.addEventListener("click", () => {
-//         deleteBook(btn.getAttribute('data-library-index'))
-//     })
-// })
 
 function refresh(){
     while (bookGrid.firstChild) {
@@ -103,16 +76,50 @@ function createBook(book){
     pages.textContent = book.pages
     let bookStatus = document.createElement("p")
     bookStatus.textContent = book.read
-    let button = document.createElement("button")
-    button.classList.add("delete-btn")
-    button.setAttribute("data-library-index", theLibrary.indexOf(book))
-    button.addEventListener("click", () => {
-        deleteBook(button.getAttribute('data-library-index'))
+    let deleteButton = document.createElement("button")
+    // delete button
+    deleteButton.classList.add("delete-btn")
+    deleteButton.setAttribute("data-library-index", theLibrary.indexOf(book))
+    deleteButton.addEventListener("click", () => {
+        deleteBook(deleteButton.getAttribute('data-library-index'))
     })
-    let icon = document.createElement("i")
-    icon.classList.add("fa-solid")
-    icon.classList.add("fa-trash-can")
-    button.appendChild(icon)
-    bookCard.append(bookImage, bookTitle, bookAuthor, pages, bookStatus, button)
+    let deleteIcon = document.createElement("i")
+    deleteIcon.classList.add("fa-solid", "fa-trash-can")
+    // edit read status
+    let editButton = document.createElement("button")
+    editButton.classList.add("edit-btn")
+    editButton.setAttribute("data-library-index", theLibrary.indexOf(book))
+    editButton.addEventListener("click", () => {
+        changeReadStatus(editButton.getAttribute('data-library-index'))
+    })
+    let editIcon = document.createElement("i")
+    editIcon.classList.add("fa-solid", "fa-pencil")
+    editButton.appendChild(editIcon)
+    deleteButton.appendChild(deleteIcon)
+    bookCard.append(bookImage, bookTitle, bookAuthor, pages, bookStatus, deleteButton, editButton)
     bookGrid.appendChild(bookCard)
 }
+
+function addBooksToLibrary(){
+    if(bookName.value.length == 0 || bookAuthor.value.length == 0 || numberOfPages.value.length == 0 || bookCompleted.value.length == 0){
+        errorMessage.textContent = "Fill in all fields" 
+        errorMessage.classList.remove("positive-e")
+    }
+    else{
+        errorMessage.textContent = "Book successfully added" 
+        errorMessage.classList.add("positive-e")
+        let book = new Book(bookName.value, bookAuthor.value, Number(numberOfPages.value), bookCompleted.value)
+        theLibrary.push(book)
+        createBook(book)
+    }
+}
+
+function changeReadStatus(bookIndex){
+    theLibrary[bookIndex].toggleReadStatus()
+    console.table(theLibrary[bookIndex])
+    refresh()
+    populateBookGrid()
+}
+
+// window loaded event
+window.addEventListener("DOMContentLoaded", populateBookGrid())
